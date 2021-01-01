e1e33fe2e4381446c66e3a87e40d5131c5437f19
Tami Black <xxx@users.noreply.github.com>
2021-01-01 10:21:22 -0800

    Initial Commit

---
+++ MissingNoodlePortfolio/MissingNoodlePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissingNoodlePortfolioApp.swift
+//  MissingNoodlePortfolio
+//
+//  Created by Tami Black on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MissingNoodlePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
