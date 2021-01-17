1dd28147348102b0ffd673d0c55a9f5c2725b125
Tami Black <missingnoodle@me.com>
2021-01-17 10:42:24 -0800

    BUG:  Fixing issue EditProjectView being dismissed and returned to the ProjectView as soon  as any state is changed.  Often noticed as soon a one single character is entered into the project name textfield.  This is apparently a bug in SwiftUI or something like that,  See:  https://www.hackingwithswift.com/forums/articles/update-forces-view-to-be-dismissed-bug-or-error-ultimateportfolio-editing-projects/4564

--- MissingNoodlePortfolio/EditProjectView.swift
+++ MissingNoodlePortfolio/EditProjectView.swift
@@ -36,7 +36,8 @@ struct EditProjectView: View {
     var body: some View {
         Form {
             Section(header: Text("Basic settings")) {
-                 TextField("Project name", text: $title, onCommit: update)
+                TextField("Project name", text: $title, onEditingChanged: { if !$0 { update() } })
+//                 TextField("Project name", text: $title.onChange(update), onCommit: update)
 //                TextField("Project name", text: $title.onChange(update))
                 TextField("Description of this project", text: $detail.onChange(update))
             }
