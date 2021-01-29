b8db1cd93f9a9a57f02b8ad07dd2d2deeee0090f
Tami Black <missingnoodle@me.com>
2021-01-29 07:04:25 -0800

    FUNCTIONALITY: Adding and deleting Items implemented.

--- MissingNoodlePortfolio/ProjectsView.swift
+++ MissingNoodlePortfolio/ProjectsView.swift
@@ -10,17 +10,21 @@ import SwiftUI
 struct ProjectsView: View {
     static let openTag: String? = "Open"
     static let closedTag: String? = "Closed"
+    
+    @EnvironmentObject var dataController: CoreDataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
     let showClosedProjects: Bool
     let projects: FetchRequest<Project>
-
+    
     init(showClosedProjects: Bool) {
         self.showClosedProjects = showClosedProjects
-
+        
         projects = FetchRequest<Project>(entity: Project.entity(),
                                          sortDescriptors: [NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)],
                                          predicate: NSPredicate(format: "closed = %d", showClosedProjects))
     }
-
+    
     var body: some View {
         NavigationView {
             List {
@@ -29,13 +33,47 @@ struct ProjectsView: View {
                         ForEach(project.projectItems) { item in
                             ItemRowView(item: item)
                         }
-                        // Or
-//                        ForEach(project.projectItems, content: ItemRowView.init) // for simple single paramater views
+                        // Or ... ForEach(project.projectItems, content: ItemRowView.init) // for simple single paramater views
+                        .onDelete { offsets in
+                            let allItems = project.projectItems // projectItems, does a lot of work, sorting, etc. we don't
+                            for offset in offsets {             // want to do all that sortng each time through the for loop below
+                                let item = allItems[offset]
+                                dataController.delete(item)
+                            }
+                            dataController.save()
+                        }
+                        
+                        if showClosedProjects == false {
+                            Button {
+                                withAnimation {
+                                    let item = Item(context: managedObjectContext)
+                                    item.project = project
+                                    item.creationDate = Date()
+                                    dataController.save()
+                                }
+                            } label: {
+                                Label("Add New Item", systemImage: "plus")
+                            }
+                        }
                     }
                 }
             }
             .listStyle(InsetGroupedListStyle())
             .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+            .toolbar {
+                if showClosedProjects == false {
+                    Button {
+                        withAnimation {
+                            let project = Project(context: managedObjectContext)
+                            project.closed = false
+                            project.creationDate = Date()
+                            dataController.save()
+                        }
+                    } label: {
+                        Label("Add Project", systemImage: "plus")
+                    }
+                }
+            }
         }
     }
 }
