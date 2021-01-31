acd49b23f740ec43391ecacca7fd6d29456bf438
Tami Black <missingnoodle@me.com>
2021-01-30 20:10:44 -0800

    FUNCTIONALITY: Adding an Awards tab to in part showcase reading JSON files from an App Bundle. Parsing data into your app is the single most common task any iOS developer needs to do, so in this article we’ll start to build out an Awards tab using JSON.

--- MissingNoodlePortfolio/ContentView.swift
+++ MissingNoodlePortfolio/ContentView.swift
@@ -18,17 +18,25 @@ struct ContentView: View {
                     Image(systemName: "house")
                     Text("Home")
                 }
+
             ProjectsView(showClosedProjects: false)
                 .tag(ProjectsView.openTag)
                 .tabItem {
                     Image(systemName: "list.bullet")
                     Text("Open")
                 }
+
             ProjectsView(showClosedProjects: true)
                 .tag(ProjectsView.closedTag)
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Closed")
+                }
+            AwardsView()
+                .tag(AwardsView.tag)
+                .tabItem {
+                    Image(systemName: "rosette")
+                    Text("Awards")
                 }
         }
     }
