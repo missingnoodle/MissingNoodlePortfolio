72e8a1ccdd5722e99d811ef6b101d00dbba68995
Tami Black <missingnoodle@me.com>
2021-02-01 06:00:09 -0800

    FUNCTIONALITY: Making the app accessible.  Adding voiceover support to the app.

--- MissingNoodlePortfolio/EditProjectView.swift
+++ MissingNoodlePortfolio/EditProjectView.swift
@@ -60,6 +60,11 @@ struct EditProjectView: View {
                             color = item
                             update()
                         }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityAddTraits(
+                            item == color ? [.isButton, .isSelected] : .isButton
+                        )
+                        .accessibilityLabel(LocalizedStringKey(item))
                     }
                 }
                 .padding(.vertical)
