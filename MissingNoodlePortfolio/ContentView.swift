1ba8c53142ea13bceb60293c0ec140907d8f9bcb
Tami Black <missingnoodle@me.com>
2021-04-25 12:48:38 -0700

    FUNCTIONALITY: Integrating with Siri shortcuts

--- MissingNoodlePortfolio/ContentView.swift
+++ MissingNoodlePortfolio/ContentView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct ContentView: View {
     @SceneStorage("selectedView") var selectedView: String?
     @EnvironmentObject var dataController: DataController
+
+    private let newProjectActivity = "com.missingnoodle.missingNoodlePortfolio.newProject"
+
     var body: some View {
         TabView(selection: $selectedView) {
             HomeView(dataController: dataController)
@@ -41,6 +44,12 @@ struct ContentView: View {
                 }
         }
         .onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome) // Spotlight
+        .onContinueUserActivity(newProjectActivity, perform: createProject) // Integrating with Shortcuts
+
+        .userActivity(newProjectActivity, { activity in
+            activity.title = "New Project"
+            activity.isEligibleForPrediction = true
+        })
         .onOpenURL(perform: openURL)
     }
 
@@ -49,6 +58,11 @@ struct ContentView: View {
     }
 
     func openURL(_ url: URL) {
+        selectedView = ProjectsView.openTag
+        dataController.addProject()
+    }
+
+    func createProject(_ userActivity: NSUserActivity) {
         selectedView = ProjectsView.openTag
         dataController.addProject()
     }
